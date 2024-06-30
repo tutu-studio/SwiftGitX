bea9f239070d3e213f30ae21131779364d189ac1
İbrahim Çetin <cetinibrahim.ci@gmail.com>
2024-06-30 21:50:30 +0300

    Add config set and get for string

--- Sources/SwiftGitX/Collections/ConfigCollection.swift
+++ Sources/SwiftGitX/Collections/ConfigCollection.swift
@@ -1,5 +1,6 @@
 import libgit2
 
+// ? Should we use actor?
 /// A collection of configurations and their operations.
 public struct ConfigCollection {
     private let repositoryPointer: OpaquePointer
@@ -25,5 +26,57 @@ public struct ConfigCollection {
         git_config_get_string_buf(&branchNameBuffer, configPointer, "init.defaultBranch")
 
         return String(cString: branchNameBuffer.ptr)
+    }
+
+    /// Sets a configuration value for the repository.
+    ///
+    /// - Parameters:
+    ///   - string: The value to set.
+    ///   - key: The key to set the value for.
+    ///
+    /// This will set the configuration value for the repository.
+    public func set(_ string: String, forKey key: String) {
+        var configPointer: OpaquePointer?
+        defer { git_config_free(configPointer) }
+
+        git_repository_config(&configPointer, repositoryPointer)
+
+        guard let configPointer else {
+            // TODO: Handle error
+            return
+        }
+
+        git_config_set_string(configPointer, key, string)
+    }
+
+    /// Returns the configuration value for the repository.
+    ///
+    /// - Parameter key: The key to get the value for.
+    ///
+    /// - Returns: The configuration value for the key.
+    ///
+    /// All config files will be looked into, in the order of their defined level. A higher level means a higher
+    /// priority. The first occurrence of the variable will be returned here.
+    public func string(forKey key: String) -> String? {
+        var configPointer: OpaquePointer?
+        defer { git_config_free(configPointer) }
+
+        git_repository_config(&configPointer, repositoryPointer)
+
+        guard let configPointer else {
+            // TODO: Handle error
+            return nil
+        }
+
+        var valueBuffer = git_buf()
+        defer { git_buf_free(&valueBuffer) }
+
+        let status = git_config_get_string_buf(&valueBuffer, configPointer, key)
+
+        guard let pointer = valueBuffer.ptr, status == GIT_OK.rawValue else {
+            return nil
+        }
+
+        return String(cString: pointer)
     }
 }
--- Tests/SwiftGitXTests/CollectionTests/ConfigCollection.swift
+++ Tests/SwiftGitXTests/CollectionTests/ConfigCollection.swift
@@ -7,4 +7,22 @@ final class ConfigCollectionTests: SwiftGitXTestCase {
 
         XCTAssertEqual(repository.config.defaultBranchName, "main")
     }
+
+    func testConfigSet() {
+        let repository = Repository.mock(named: "test-config-set", in: Self.directory)
+
+        repository.config.set("develop", forKey: "init.defaultBranch")
+
+        XCTAssertEqual(repository.config.defaultBranchName, "develop")
+    }
+
+    func testConfigString() {
+        let repository = Repository.mock(named: "test-config-string", in: Self.directory)
+
+        repository.config.set("İbrahim Çetin", forKey: "user.name")
+        repository.config.set("mail@ibrahimcetin.dev", forKey: "user.email")
+
+        XCTAssertEqual(repository.config.string(forKey: "user.name"), "İbrahim Çetin")
+        XCTAssertEqual(repository.config.string(forKey: "user.email"), "mail@ibrahimcetin.dev")
+    }
 }
